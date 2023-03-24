8a48e205d5a317bfb3e031d49e179d57acc2a4c7
Bcmukundi <mbournventure@gmail.com>
2023-03-24 04:00:53 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,5 @@
 int _putchar(char c);
 
-int _isupper(int c);
-
 int _isdigit(int c);
 
 int mul(int a, int b);
@@ -21,4 +19,3 @@ void print_square(int size);
 void print_triangle(int size);
 
 void print_number(int n);
-~                                 
