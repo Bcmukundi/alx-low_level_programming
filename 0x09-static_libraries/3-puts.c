c38f17ae959b035a413a00604edf98350ac728ee
Bcmukundi <mbournventure@gmail.com>
2023-06-13 05:40:41 -0700

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+void _puts(char *str)
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
